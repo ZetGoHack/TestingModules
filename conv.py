5ce8b6cbd7ef7647970bbbf1edd9418e1487d75a
ZetGo <73279716+ZetGoHack@users.noreply.github.com>
2025-03-08 14:30:07 +0200

    Create conv.py

---
+++ conv.py
@@ -0,0 +1,17 @@
+from .. import loader, utils
+import asyncio
+@loader.tds
+class Test(loader.Module):
+    """A"""
+    strings = {"name": "Conv"}
+    @loader.command
+    async def conv(self):
+        """B"""
+        async with self._client.conversation(7212151458) as conv:
+            await conv.send_message("сюда")
+            try:
+                r = await conv.get_response()
+            except:
+                pass
+            await conv.send_message(r)
+    
