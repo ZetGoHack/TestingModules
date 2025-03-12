174e5c7e9471d46249e9255a1eff09eaf6e80785
ZetGo <73279716+ZetGoHack@users.noreply.github.com>
2025-03-12 21:56:20 +0200

    Update Chess.py

--- Chess.py
+++ Chess.py
@@ -36,84 +36,84 @@ class chess(loader.Module):
         self.inline.form(message = message, text = "‌", reply_markup = 
             [
                 [
-                {"text": f"{board["A1"]}", "callback": yea},
-                {"text": f"{board["B1"]}", "callback": yea},
-                {"text": f"{board["C1"]}", "callback": yea},
-                {"text": f"{board["D1"]}", "callback": yea},
-                {"text": f"{board["E1"]}", "callback": yea},
-                {"text": f"{board["F1"]}", "callback": yea},
-                {"text": f"{board["G1"]}", "callback": yea},
-                {"text": f"{board["H1"]}", "callback": yea}
+                    {"text": f"{board['A1']}", "callback": yea},
+                    {"text": f"{board['B1']}", "callback": yea},
+                    {"text": f"{board['C1']}", "callback": yea},
+                    {"text": f"{board['D1']}", "callback": yea},
+                    {"text": f"{board['E1']}", "callback": yea},
+                    {"text": f"{board['F1']}", "callback": yea},
+                    {"text": f"{board['G1']}", "callback": yea},
+                    {"text": f"{board['H1']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A2"]}", "callback": yea},
-                {"text": f"{board["B2"]}", "callback": yea},
-                {"text": f"{board["C2"]}", "callback": yea},
-                {"text": f"{board["D2"]}", "callback": yea},
-                {"text": f"{board["E2"]}", "callback": yea},
-                {"text": f"{board["F2"]}", "callback": yea},
-                {"text": f"{board["G2"]}", "callback": yea},
-                {"text": f"{board["H2"]}", "callback": yea}
+                    {"text": f"{board['A2']}", "callback": yea},
+                    {"text": f"{board['B2']}", "callback": yea},
+                    {"text": f"{board['C2']}", "callback": yea},
+                    {"text": f"{board['D2']}", "callback": yea},
+                    {"text": f"{board['E2']}", "callback": yea},
+                    {"text": f"{board['F2']}", "callback": yea},
+                    {"text": f"{board['G2']}", "callback": yea},
+                    {"text": f"{board['H2']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A3"]}", "callback": yea},
-                {"text": f"{board["B3"]}", "callback": yea},
-                {"text": f"{board["C3"]}", "callback": yea},
-                {"text": f"{board["D3"]}", "callback": yea},
-                {"text": f"{board["E3"]}", "callback": yea},
-                {"text": f"{board["F3"]}", "callback": yea},
-                {"text": f"{board["G3"]}", "callback": yea},
-                {"text": f"{board["H3"]}", "callback": yea}
+                    {"text": f"{board['A3']}", "callback": yea},
+                    {"text": f"{board['B3']}", "callback": yea},
+                    {"text": f"{board['C3']}", "callback": yea},
+                    {"text": f"{board['D3']}", "callback": yea},
+                    {"text": f"{board['E3']}", "callback": yea},
+                    {"text": f"{board['F3']}", "callback": yea},
+                    {"text": f"{board['G3']}", "callback": yea},
+                    {"text": f"{board['H3']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A4"]}", "callback": yea},
-                {"text": f"{board["B4"]}", "callback": yea},
-                {"text": f"{board["C4"]}", "callback": yea},
-                {"text": f"{board["D4"]}", "callback": yea},
-                {"text": f"{board["E4"]}", "callback": yea},
-                {"text": f"{board["F4"]}", "callback": yea},
-                {"text": f"{board["G4"]}", "callback": yea},
-                {"text": f"{board["H4"]}", "callback": yea}
+                    {"text": f"{board['A4']}", "callback": yea},
+                    {"text": f"{board['B4']}", "callback": yea},
+                    {"text": f"{board['C4']}", "callback": yea},
+                    {"text": f"{board['D4']}", "callback": yea},
+                    {"text": f"{board['E4']}", "callback": yea},
+                    {"text": f"{board['F4']}", "callback": yea},
+                    {"text": f"{board['G4']}", "callback": yea},
+                    {"text": f"{board['H4']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A5"]}", "callback": yea},
-                {"text": f"{board["B5"]}", "callback": yea},
-                {"text": f"{board["C5"]}", "callback": yea},
-                {"text": f"{board["D5"]}", "callback": yea},
-                {"text": f"{board["E5"]}", "callback": yea},
-                {"text": f"{board["F5"]}", "callback": yea},
-                {"text": f"{board["G5"]}", "callback": yea},
-                {"text": f"{board["H5"]}", "callback": yea}
+                    {"text": f"{board['A5']}", "callback": yea},
+                    {"text": f"{board['B5']}", "callback": yea},
+                    {"text": f"{board['C5']}", "callback": yea},
+                    {"text": f"{board['D5']}", "callback": yea},
+                    {"text": f"{board['E5']}", "callback": yea},
+                    {"text": f"{board['F5']}", "callback": yea},
+                    {"text": f"{board['G5']}", "callback": yea},
+                    {"text": f"{board['H5']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A6"]}", "callback": yea},
-                {"text": f"{board["B6"]}", "callback": yea},
-                {"text": f"{board["C6"]}", "callback": yea},
-                {"text": f"{board["D6"]}", "callback": yea},
-                {"text": f"{board["E6"]}", "callback": yea},
-                {"text": f"{board["F6"]}", "callback": yea},
-                {"text": f"{board["G6"]}", "callback": yea},
-                {"text": f"{board["H6"]}", "callback": yea}
+                    {"text": f"{board['A6']}", "callback": yea},
+                    {"text": f"{board['B6']}", "callback": yea},
+                    {"text": f"{board['C6']}", "callback": yea},
+                    {"text": f"{board['D6']}", "callback": yea},
+                    {"text": f"{board['E6']}", "callback": yea},
+                    {"text": f"{board['F6']}", "callback": yea},
+                    {"text": f"{board['G6']}", "callback": yea},
+                    {"text": f"{board['H6']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A7"]}", "callback": yea},
-                {"text": f"{board["B7"]}", "callback": yea},
-                {"text": f"{board["C7"]}", "callback": yea},
-                {"text": f"{board["D7"]}", "callback": yea},
-                {"text": f"{board["E7"]}", "callback": yea},
-                {"text": f"{board["F7"]}", "callback": yea},
-                {"text": f"{board["G7"]}", "callback": yea},
-                {"text": f"{board["H7"]}", "callback": yea}
+                    {"text": f"{board['A7']}", "callback": yea},
+                    {"text": f"{board['B7']}", "callback": yea},
+                    {"text": f"{board['C7']}", "callback": yea},
+                    {"text": f"{board['D7']}", "callback": yea},
+                    {"text": f"{board['E7']}", "callback": yea},
+                    {"text": f"{board['F7']}", "callback": yea},
+                    {"text": f"{board['G7']}", "callback": yea},
+                    {"text": f"{board['H7']}", "callback": yea}
                 ],
                 [
-                {"text": f"{board["A8"]}", "callback": yea},
-                {"text": f"{board["B8"]}", "callback": yea},
-                {"text": f"{board["C8"]}", "callback": yea},
-                {"text": f"{board["D8"]}", "callback": yea},
-                {"text": f"{board["E8"]}", "callback": yea},
-                {"text": f"{board["F8"]}", "callback": yea},
-                {"text": f"{board["G8"]}", "callback": yea},
-                {"text": f"{board["H8"]}", "callback": yea}
+                    {"text": f"{board['A8']}", "callback": yea},
+                    {"text": f"{board['B8']}", "callback": yea},
+                    {"text": f"{board['C8']}", "callback": yea},
+                    {"text": f"{board['D8']}", "callback": yea},
+                    {"text": f"{board['E8']}", "callback": yea},
+                    {"text": f"{board['F8']}", "callback": yea},
+                    {"text": f"{board['G8']}", "callback": yea},
+                    {"text": f"{board['H8']}", "callback": yea}
                 ]
             ]
         )
