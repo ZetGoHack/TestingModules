e926f7c5f4779ccb9b99fefc4713e76cb1476c63
ZetGo <73279716+ZetGoHack@users.noreply.github.com>
2025-03-24 10:07:24 +0400

    Update .NwGifH.py

--- .NwGifH.py
+++ .NwGifH.py
@@ -96,7 +96,7 @@ class GifHarem(loader.Module):
     async def check_loop(self):
         await self.client.send_message("me",f"Запустились\n{self.get('ABonud_timeG')} {self.config['abG']}")
         if self.config["abG"]:
-            await self.client.send_message("me",f"Включены\n{self.get('ABonud_timeG')} {self.config['abG']} след блок: {not self.get("ABonus_timeG")} или {(time.time() - self.get("ABonus_timeG")) >= 3600*4} должны дать (not self.get("ABonus_timeG") or (time.time() - self.get("ABonus_timeG")) >= 3600*4)")
+            await self.client.send_message("me",f"Включены\n{self.get('ABonud_timeG')} {self.config['abG']} след блок: {not self.get('ABonus_timeG')} или {(time.time() - self.get('ABonus_timeG')) >= 3600*4} должны дать {(not self.get('ABonus_timeG') or (time.time() - self.get('ABonus_timeG')) >= 3600*4)}")
             if (not self.get("ABonus_timeG") or (time.time() - self.get("ABonus_timeG")) >= 3600*4):
                 await self.client.send_message("me", f"работаем-работаем {self.get('ABonud_timeG')}")
                 await self.autobonus()
