7fc328d431760d9e72f4d5305cc9ca11e639731b
ZetGo | Aleksej K. <ZetGoHack@users.noreply.github.com>
2025-06-12 00:01:35 +0300

    I think that's all

--- harem_manager.py
+++ harem_manager.py
@@ -227,7 +227,8 @@ class HaremManager(loader.Module):
     async def _set_menu(self, message):
         await utils.answer(
             message,
-            "❤️ Выберите бота для управления\n\n<i>*✅ Означает, что автобонус включён</i>",
+            "❤️ Выберите бота для управления\n\n✅ <i>- означает, что автобонус включён</i>\
+\nБольше настроек в конфиге модуля(<code>.config HaremManager</code>)",
             reply_markup=self._main_markup()
         )
 
