f39e28dda3435828191d2d3bdc94b98179285b20
ZetGo | Aleksej K. <73279716+ZetGoHack@users.noreply.github.com>
2025-07-01 21:12:49 +0300

    Create twst2.py

---
+++ twst2.py
@@ -0,0 +1,9 @@
+__version__ = (2,5,8)
+from .. import loader
+@loader.tds
+class impossible(loader.Module): 
+ """Test"""
+ strings={"name":"impossible"}
+ async def asderfrtgcmd(self,m):
+  """com"""#and
+  pass
