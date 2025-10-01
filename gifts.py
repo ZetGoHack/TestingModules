a29eee47adc479cd4df697a7cce7f02c4bb19235
ZetGo | Aleksej K. <ZetGoHack@users.noreply.github.com>
2025-10-01 17:44:27 +0300

    gifts

--- gifts.py
+++ gifts.py
@@ -4,19 +4,22 @@
 #░░░█░░░█░░░░█░░█░█░█░█
 #░░░███░███░░█░░███░███
 #H:Mods Team [💎]
-v = ("oooo", "kaaa", "kkk")
-# meta developer: @nullmod
+v = ("oooo", "kaaa", "kkkk")
+# meta developer: @nullmod & @codermasochist
 # scope: heroku_min 1.7.2
 # scope: hikka_min 1.7.2
 
-# -      main      - #
 from .. import loader, utils
 # -      func      - #
 import asyncio
 import time
 from herokutl.tl.functions.payments import GetSavedStarGiftsRequest, GetUniqueStarGiftRequest
 # -      types     - #
-from herokutl.tl.types import Channel, SavedStarGift, StarGift, StarGiftUnique, User
+from herokutl.tl.types import (
+    Channel, User, SavedStarGift, StarGift,
+    StarGiftUnique, StarGiftAttributeModel,
+    StarGiftAttributePattern, StarGiftAttributeBackdrop
+)
 # -      error       - #
 from herokutl.errors.rpcerrorlist import DocumentInvalidError
 # -      end       - #
@@ -258,300 +261,65 @@ class Gifts(loader.Module):
     @loader.command(ru_doc="[ссылка/gift-id] посмотреть информацию о подарке")
     async def gift(self, message):
         """[link/gift-id] view gift info"""
-        args = utils.get_args_raw(message).replace("t.me/nft/", "").split()
+        args = utils.get_args_raw(message)
         if not args:
             await utils.answer(message, self.strings["noargs"])
             return
-        if len(args) > 1:
-            await utils.answer(message, self.strings["toomany"])
+            
+        slug = args
+        if "t.me/nft/" in args:
+            slug = args.replace("https://t.me/nft/", "").replace("http://t.me/nft/", "").replace("t.me/nft/", "")
+        
+        slug = slug.split()[0].split('?')[0]
+        
+        if not slug:
+            await utils.answer(message, self.strings["noargs"])
             return
+
         try:
-            nft = await self.client(GetUniqueStarGiftRequest(slug=args[0]))
+            res = await self.client(GetUniqueStarGiftRequest(slug=slug))
         except Exception as e:
             if "STARGIFT_SLUG_INVALID" in str(e):
                 await utils.answer(message, self.strings["gifterr"])
                 return
-        text = f"<a href='t.me/nft/{args[0]}'>\u200f</a>Окак (команда в процессе разработки не нужно это того самое)"
-        await utils.answer(message, text)#, invert_media=True)
+            raise
 
-    async def _split_nfts(self, gifts):
-        strings = []
-        for gift in gifts:
-            strings.append(
-                self.strings["nft"]
-            )
+        owner = res.users[0] if res.users else None
+        if owner:
+            first_name = getattr(owner, "first_name", "") or ""
+            last_name = getattr(owner, "last_name", "") or ""
+            owner_display_name = f"{first_name} {last_name}".strip() or "Unknown"
+            if getattr(owner, "username", None) or (getattr(owner, "usernames", None) and len(owner.usernames) > 0):
+                username = owner.username if getattr(owner, "username", None) else owner.usernames[0].username
+                owner_str = f'<emoji document_id=5275979556308674886>👤</emoji> <b>Owner</b>: <a href="https://t.me/{username}">{owner_display_name}</a> (id: <code>{owner.id}</code>)'
+            else:
+                owner_str = f'<emoji document_id=5275979556308674886>👤</emoji> <b>Owner</b>: {owner_display_name} (id: <code>{owner.id}</code>)'
+        elif getattr(res.gift, "owner_name", None):
+            owner_display_name = res.gift.owner_name
+            owner_str = f'<emoji document_id=5275979556308674886>👤</emoji> <b>Owner</b>: {owner_display_name}'
+        else:
+            owner_str = f'<emoji document_id=5275979556308674886>👤</emoji> <b>Owner</b>: Unknown'
 
+        lines = [owner_str]
+
+        for g in res.gift.attributes:
+            if hasattr(g, "name"):
+                if isinstance(g, StarGiftAttributeModel):
+                    attr_type = f'<emoji document_id="{res.gift.attributes[0].document.id}">🎁</emoji> <b>Model</b>'
+                elif isinstance(g, StarGiftAttributePattern):
+                    attr_type = "<emoji document_id=5253944419870062295>🍃</emoji> <b>Pattern</b>"
+                elif isinstance(g, StarGiftAttributeBackdrop):
+                    attr_type = "<emoji document_id=5764899533565729469>🎨</emoji> <b>Background</b>"
+                else:
+                    attr_type = "Attribute"
+                lines.append(f"{attr_type}: <code>{g.name}</code> (<code>{getattr(g, 'rarity_permille', 0)/10:.1f}</code>%)")
+
+        lines.append(f"<emoji document_id=6007817446398890097>📝</emoji> <b>Issued</b>: <code>{res.gift.availability_issued}</code> / <code>{res.gift.availability_total}</code>")
         
-__version__ = v
+        if hasattr(res.gift, 'value_amount') and hasattr(res.gift, 'value_currency'):
+            lines.append(f"<emoji document_id=6014655953457123498>💱</emoji> <b>Price</b>: <code>{res.gift.value_amount // 100}</code> {res.gift.value_currency}")
 
-# nft = UniqueStarGift(
-#  gift=StarGiftUnique(
-#   id=5821049767035142988,
-#   title='Trapped Heart',
-#   slug='TrappedHeart-19095',
-#   num=19095,
-#   attributes=[
-#    StarGiftAttributeModel(
-#     name='Silver Luxe',
-#     document=Document(
-#      id=5402510413835302585,
-#      access_hash=-6433169821750188931,
-#      file_reference=b'\x00hC\xf1\xe3v\x9b\xcc\x11\x03:\xcdy\x1a\x12GZ$\x02\xbe\x1b',
-#      date=datetime.datetime(2024, 12, 16, 1, 40, 52, tzinfo=datetime.timezone.utc),
-#      mime_type='application/x-tgsticker',
-#      size=18196,
-#      dc_id=2,
-#      attributes=[
-#       DocumentAttributeImageSize(
-#        w=512,
-#        h=512
-#       ),
-#       DocumentAttributeCustomEmoji(
-#        alt='🖤',
-#        stickerset=InputStickerSetID(
-#         id=1260742488159682581,
-#         access_hash=-9108761878258913401
-#        ),
-#        free=False,
-#        text_color=False
-#       ),
-#       DocumentAttributeFilename(
-#        file_name='AnimatedSticker.tgs'
-#       ),
-#      ],
-#      thumbs=[
-#       PhotoPathSize(
-#        type='j',
-#        bytes=b'\x1b\t\xb2\x04\xdc\\b~F\x07K\x01G\x02[Ct\x87\x03F\x08\x86\x06PF\x90`\x91g\x81EJVMZP\\gtF\x06F\x08KGa\x88e\x80FN\x94L\x9c`\x8de\x96G\x06\x9aK\x06\x84n\x81I\x04JN\x00CLpp`v\x84B\xb1\xad\x88\x00\xb0\xa2\x84\x87\x04G\x8a\x02\\\x8aG\xa3m\xa9k\x88\x83\x8f\xa0\x95\xa8\x98\x9d\xbc\xae\x89\x06\xb6\xa4\x87\x87\x05\x85\x8a\tJ\x88C\xaca\xb1U\x85\x8eW\x95\\\x9fF\x8eI\x9fK\xaeE\xadC\x89\x08\x8d\x8e\x01\x88\x97\x9f\x8e\x9b\x9eB\x89S\x80W\x81C\x80D\x85E\x87S\x9e^\x86\x08X\x8a\x03\x81\x8b\x82\x9a\x87\xa3\x84\x89\x9e\x9b\x9a\xa5H\x95aWf\\HHWMaOhGI\x01\x89L\x02\xa3G\x86M\x8fU\x94'
-#       ),
-#       PhotoSize(
-#        type='m',
-#        w=128,
-#        h=128,
-#        size=7734
-#       ),
-#      ],
-#      video_thumbs=[
-#      ]
-#     ),
-#     rarity_permille=20
-#    ),
-#    StarGiftAttributePattern(
-#     name='Beetle',
-#     document=Document(
-#      id=5440627552802592767,
-#      access_hash=-642335644292992144,
-#      file_reference=b'\x00hC\xf1\xe3\xc7:l,f\xff\xe7\xac\x94 qc\x07\xb1\xe7\xcd',
-#      date=datetime.datetime(2024, 12, 28, 11, 8, 38, tzinfo=datetime.timezone.utc),
-#      mime_type='application/x-tgsticker',
-#      size=1265,
-#      dc_id=2,
-#      attributes=[
-#       DocumentAttributeImageSize(
-#        w=512,
-#        h=512
-#       ),
-#       DocumentAttributeCustomEmoji(
-#        alt='🪲',
-#        stickerset=InputStickerSetID(
-#         id=1983652455501529109,
-#         access_hash=3008396452719838757
-#        ),
-#        free=False,
-#        text_color=True
-#       ),
-#       DocumentAttributeFilename(
-#        file_name='AnimatedSticker.tgs'
-#       ),
-#      ],
-#      thumbs=[
-#       PhotoPathSize(
-#        type='j',
-#        bytes=b'\t\x05\xaf\x07\xdcG\x04c\xa5O\x05\xacT\x05\x86k[\x92j\x9cE\x83f\x8cg\x91C\x8eS\x86\x08g\x9daF\x07\x8e\x06L\x08\x8e\x08P\x04\x80JTIZQKOPl`vYOG\x02\x8bvk\x83I\x89S\x91W\xa3Q\x88\x04\x8c\t\x8d\x08\x88\x04\x8cJNIWTHIeH\x02eH\x03\x87H\x95J\x9fJ\xbf\x80\x83\xbc\xb9\xb9\x93A\xa8\x84\xbb\x81\x8fBNI\x08\xbcs\x86\x84V\x87\x04`\x88\x06G\x88Y\x88^\x8eD\x86\x8b\x8a\x91\x8e\xa8\x9d\x8a\x06L\t\x8d\x08H\x04\xa9\xbaX\xaco\xbaR\x8bV\xabd\xbaF\x87V\x85V\x8d\x82\xa4\x92\x08\x8a\x00\x8e\x05\x90\x08T\xa1bUda\x81EePjTPLWbhkQH\x83\xa9\x91\xb4\x90\x8c\x88\x02\x8f\x08\xa1\x91\x01_\x88mM\x00uK\x04Z\xb3J\x04\x92\x00J\x05\xb4\x80t\x81J\x04CO\x06AW\x83I\x02FG\x00U\xb3\x8c\x99\x05L\x9c\x00b\xa4J\x01J\x05J\x01J\x05T\xa2O\x8a\x01o\x8b\x03'
-#       ),
-#       PhotoSize(
-#        type='m',
-#        w=128,
-#        h=128,
-#        size=2970
-#       ),
-#      ],
-#      video_thumbs=[
-#      ]
-#     ),
-#     rarity_permille=2
-#    ),
-#    StarGiftAttributeBackdrop(
-#     name='Satin Gold',
-#     backdrop_id=19,
-#     center_color=12557127,
-#     edge_color=9271097,
-#     pattern_color=6109952,
-#     text_color=16704681,
-#     rarity_permille=15
-#    ),
-#    StarGiftAttributeOriginalDetails(
-#     recipient_id=PeerUser(
-#      user_id=1226061708
-#     ),
-#     date=datetime.datetime(2024, 10, 29, 21, 24, 56, tzinfo=datetime.timezone.utc),
-#     sender_id=PeerUser(
-#      user_id=7212151458
-#     ),
-#     message=None
-#    ),
-#   ],
-#   availability_issued=23355,
-#   availability_total=26407,
-#   owner_id=PeerUser(
-#    user_id=1226061708
-#   ),
-#   owner_name=None,
-#   owner_address=None,
-#   gift_address=None,
-#   resell_stars=None
-#  ),
-#  users=[
-#   User(
-#    id=1226061708,
-#    is_self=True,
-#    contact=True,
-#    mutual_contact=True,
-#    deleted=False,
-#    bot=False,
-#    bot_chat_history=False,
-#    bot_nochats=False,
-#    verified=False,
-#    restricted=False,
-#    min=False,
-#    bot_inline_geo=False,
-#    support=False,
-#    scam=False,
-#    apply_min_photo=False,
-#    fake=False,
-#    bot_attach_menu=False,
-#    premium=True,
-#    attach_menu_enabled=False,
-#    bot_can_edit=False,
-#    close_friend=False,
-#    stories_hidden=False,
-#    stories_unavailable=True,
-#    contact_require_premium=False,
-#    bot_business=False,
-#    bot_has_main_app=False,
-#    access_hash=7904001167064024655,
-#    first_name='\u200f\u2067\u2067\u2067\u2067 стик\u2067Пуши',
-#    last_name=None,
-#    username=None,
-#    phone='&lt;phone&gt;',
-#    photo=UserProfilePhoto(
-#     photo_id=5332626249799039002,
-#     dc_id=2,
-#     has_video=False,
-#     personal=False,
-#     stripped_thumb=b'\x01\x08\x08\x81\xdb\x01O;\x8eh\xa2\x8ab?'
-#    ),
-#    status=UserStatusOnline(
-#     expires=datetime.datetime(2025, 6, 7, 8, 6, 19, tzinfo=datetime.timezone.utc)
-#    ),
-#    bot_info_version=None,
-#    restriction_reason=[
-#    ],
-#    bot_inline_placeholder=None,
-#    lang_code=None,
-#    emoji_status=EmojiStatusCollectible(
-#     collectible_id=6032857814404170672,
-#     document_id=5425029528663646777,
-#     title='Eternal Rose #2438',
-#     slug='EternalRose-2438',
-#     pattern_document_id=5308032304533223255,
-#     center_color=13413185,
-#     edge_color=9993010,
-#     pattern_color=7355392,
-#     text_color=16770475,
-#     until=None
-#    ),
-#    usernames=[
-#     Username(
-#      username='ZetGo',
-#      editable=True,
-#      active=True
-#     ),
-#     Username(
-#      username='tgfurr',
-#      editable=False,
-#      active=True
-#     ),
-#    ],
-#    stories_max_id=None,
-#    color=PeerColor(
-#     color=11,
-#     background_emoji_id=5368841297618547822
-#    ),
-#    profile_color=PeerColor(
-#     color=15,
-#     background_emoji_id=5202021924573561302
-#    ),
-#    bot_active_users=None,
-#    bot_verification_icon=None,
-#    send_paid_messages_stars=None
-#   ),
-#   User(
-#    id=7212151458,
-#    is_self=False,
-#    contact=True,
-#    mutual_contact=True,
-#    deleted=False,
-#    bot=False,
-#    bot_chat_history=False,
-#    bot_nochats=False,
-#    verified=False,
-#    restricted=False,
-#    min=False,
-#    bot_inline_geo=False,
-#    support=False,
-#    scam=False,
-#    apply_min_photo=True,
-#    fake=False,
-#    bot_attach_menu=False,
-#    premium=False,
-#    attach_menu_enabled=False,
-#    bot_can_edit=False,
-#    close_friend=False,
-#    stories_hidden=False,
-#    stories_unavailable=True,
-#    contact_require_premium=False,
-#    bot_business=False,
-#    bot_has_main_app=False,
-#    access_hash=-795669221529757911,
-#    first_name='Мими 💖 основа @nekoFwU',
-#    last_name=None,
-#    username='Last_Mimi',
-#    phone='37060444231',
-#    photo=UserProfilePhoto(
-#     photo_id=5967736142033962458,
-#     dc_id=4,
-#     has_video=False,
-#     personal=False,
-#     stripped_thumb=b'\x01\x08\x08h\x8e]\xc1\xfe}\xfb\xb9\x18\xe3\x14QE$S?'
-#    ),
-#    status=UserStatusOffline(
-#     was_online=datetime.datetime(2025, 6, 6, 16, 9, 37, tzinfo=datetime.timezone.utc)
-#    ),
-#    bot_info_version=None,
-#    restriction_reason=[
-#    ],
-#    bot_inline_placeholder=None,
-#    lang_code=None,
-#    emoji_status=None,
-#    usernames=[
-#    ],
-#    stories_max_id=None,
-#    color=None,
-#    profile_color=None,
-#    bot_active_users=None,
-#    bot_verification_icon=None,
-#    send_paid_messages_stars=None
-#   ),
-#  ]
-# )
+        result = "\n".join(lines)
+        await utils.answer(message, f'<a href="t.me/nft/{slug}">\u200f</a><blockquote>{result}</blockquote>', invert_media=True)
+
+__version__ = v
