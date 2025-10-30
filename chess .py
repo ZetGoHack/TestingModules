b1460e6ceb68e3537446e9d86281f58969b7ab72
ZetGo | Aleksej K. <73279716+ZetGoHack@users.noreply.github.com>
2025-10-30 11:30:34 +0200

    not chess

--- chess .py
+++ chess .py
@@ -12,18 +12,15 @@ game.headers["White"] = "%s" % (engine.id)
 game.headers["Black"] = "%s" % (engine.id)
 game.headers["Date"] = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 moveIndex=0
-node=None
+node=game
 while not board.is_game_over():
-    result = engine.play(board, chess.engine.Limit(time=0.1))
+    result = engine.play(board, chess.engine.Limit(time=0.05))
     move=str(result.move)
     board.push(result.move)
-    print ("%d-%s: %s" % (moveIndex//2+1,"white" if moveIndex%2==0 else "black",move))
-    print (board.unicode())
-    if moveIndex == 0:
-        node = game.add_variation(chess.Move.from_uci(move))
-    else:
-        node = node.add_variation(chess.Move.from_uci(move))
+    print("%d-%s: %s" % (moveIndex//2+1,"white" if moveIndex%2==0 else "black",move))
+    print(board.unicode())
+    node = node.add_variation(chess.Move.from_uci(move))
     moveIndex+=1
 
 engine.quit()    
-print (game)   
+print(game)
