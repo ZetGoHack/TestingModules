f7ea3c064f586e88ed2d1b4dc50335dddd24b09f
ZetGo | Aleksej K. <ZetGoHack@users.noreply.github.com>
2025-11-23 14:28:57 +0200

    chess - uncomment stockfish

--- chess .py
+++ chess .py
@@ -4,17 +4,18 @@ import chess.engine
 import chess.pgn
 
 engine = chess.engine.SimpleEngine.popen_uci("C:\\Users\\aleks\\Downloads\\stockfish-windows-x86-64-avx2\\stockfish\\stockfish-windows-x86-64-avx2.exe")
+engine.configure({"UCI_LimitStrength": True, "UCI_Elo": 1400})
 board = chess.Board()
 game = chess.pgn.Game() 
 game.headers["Event"] = "test_chessengine %s" % (engine.id)
 game.headers["Site"] = "http://wiki.bitplan.com/index.php/PlayChessWithAWebCam" 
-game.headers["White"] = "%s" % (engine.id)
-game.headers["Black"] = "%s" % (engine.id)
+game.headers["White"] = "%s" % (engine.id["name"])
+game.headers["Black"] = "%s" % (engine.id["name"])
 game.headers["Date"] = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 moveIndex=0
 node=game
 while not board.is_game_over():
-    result = engine.play(board, chess.engine.Limit(time=0.05))
+    result = engine.play(board, chess.engine.Limit(time=0.1))
     move=str(result.move)
     board.push(result.move)
     print("%d-%s: %s" % (moveIndex//2+1,"white" if moveIndex%2==0 else "black",move))
@@ -23,4 +24,4 @@ while not board.is_game_over():
     moveIndex+=1
 
 engine.quit()    
-print(game.accept(chess.pgn.StringExporter(columns=16, headers=False)))
+print(game)
