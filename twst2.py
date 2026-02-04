c0e5c76940df0be23274c48fde8a56c4cdb7232c
ZetGo | Aleksej K. <ZetGoHack@users.noreply.github.com>
2026-02-05 01:02:38 +0200

    remove some files

--- twst2.py
+++
@@ -1,9 +0,0 @@
-__version__ = (2,5,8)
-from .. import loader
-@loader.tds
-class impossible(loader.Module): 
- """Test"""
- strings={"name":"impossible"}
- async def asderfrtgcmd(self,m):
-  """com"""#and
-  pass
